323e4220ab479292772cd7d51d33ae056d90ec13
David Chase <davidchase03@gmail.com>
2016-06-25 15:21:36 -0400

    simplify

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const {flip, over, lens, update, nth, useWith, equals, length, ifElse, join, head, compose, converge, replace, identity, split} = require('ramda');
+const {compose, toLower, replace, append, decode} = require('./prelude');
 
-const overF = flip(over);
-const lengthLens = xs => lens(nth(xs.length - 1), update(xs.length -1));
-const splitJoinPattern = '://';
-const doubleSlashes = /\/{2,}/g;
-const multiDots = /\/\.{1,}/g;
-const listLengthEq = useWith(equals, [identity, length]);
-const joinOrHead = ifElse(listLengthEq(2), join(splitJoinPattern), head);
-const dedupeUrl = compose(replace(doubleSlashes, '/'), replace(multiDots, '/'));
-
-exports.normalize = compose(joinOrHead, converge(overF(dedupeUrl), [lengthLens, identity]), split(splitJoinPattern));
+exports.normalize = compose([replace(/([^:]\/)\/+/g, '$1'),
+    replace(/\/\.+/g, '/'),
+    replace(/(\:80|\:443)/, ''),
+    toLower,
+    append('/'),
+    decode
+]);
