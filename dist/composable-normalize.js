b6ea62ef6272bb9f692919aa6fbd4b49f01935bf
David Chase <davidchase03@gmail.com>
2016-06-25 22:48:35 -0400

    beautify :sparkles:

--- dist/composable-normalize.js
+++ dist/composable-normalize.js
@@ -1,31 +1,47 @@
-(function (global, factory) {
+(function(global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define('composableNormalize', factory) :
-    (global.composableNormalize = factory());
-}(this, function () { 'use strict';
+        typeof define === 'function' && define.amd ? define('composableNormalize', factory) :
+        (global.composableNormalize = factory());
+}(this, function() {
+    'use strict';
 
     // decode :: String -> String
     var decode = decodeURIComponent;
 
     // _compose :: (b -> c) -> (a -> b) -> (a -> c)
-    var _compose = function (f, g) { return function () {
-        var args = [], len = arguments.length;
-        while ( len-- ) args[ len ] = arguments[ len ];
+    var _compose = function(f, g) {
+        return function() {
+            var args = [],
+                len = arguments.length;
+            while (len--) args[len] = arguments[len];
 
-        return f(g.apply(void 0, args));
-; }        };
+            return f(g.apply(void 0, args));;
+        }
+    };
 
     // compose :: [(d -> e) -> (c -> d) -> (b -> c) -> (a -> b)] -> (a -> e)
-    var compose = function (fns) { return fns.reduce(_compose); };
+    var compose = function(fns) {
+        return fns.reduce(_compose);
+    };
 
     // replace :: Regex -> String -> String -> String
-    var replace = function (pattern, substr) { return function (str) { return str.replace(pattern, substr); }; };
+    var replace = function(pattern, substr) {
+        return function(str) {
+            return str.replace(pattern, substr);
+        };
+    };
 
     // toLower :: String -> String
-    var toLower = function (str) { return str.toLowerCase(); };
+    var toLower = function(str) {
+        return str.toLowerCase();
+    };
 
     // append :: String -> String -> String
-    var append = function (a) { return function (b) { return b.concat(a); }; };
+    var append = function(a) {
+        return function(b) {
+            return b.concat(a);
+        };
+    };
 
     var dedupeSlashes = /([^:]\/)\/+/g;
     var dedupeDots = /\/\.+/g;
